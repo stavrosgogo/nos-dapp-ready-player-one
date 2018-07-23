584640af767a789bcc380c603b0087fd606a83c3
stavrosgogo <41300069+stavrosgogo@users.noreply.github.com>
2018-07-23 22:16:03 +0300

    first push
    first push

---
+++ src/components/GameInfo.jsx
@@ -0,0 +1,51 @@
+import React, { PropTypes } from 'react';
+
+
+const style = () => {
+    return {
+        container: {
+            textAlign: 'center'
+        },
+        info: {
+            display: 'flex',
+            flexFlow: 'row nowrap',
+            justifyContent: 'space-around'
+        }
+    };
+};
+
+const GameInfo = ({
+    timeElapsed,
+    playerScore,
+    highScore
+}) => {
+    const { container, info } = style();
+    return (
+        <div style={container}>
+            <h3>Use arrows to move.</h3>
+            <div style={info}>
+                <p>Time: {timeElapsed}</p>
+                <p>Score: {playerScore}</p>
+            </div>
+            <div style={info}>
+                <p>High Score: {highScore}</p>
+           
+        <button onClick={() => this.handleAlert(nos.send(send))}>
+          Donate Gas
+        </button>
+
+
+            </div>
+
+        </div>
+    )
+}
+
+GameInfo.propTypes = {
+    timeElapsed: PropTypes.number.isRequired,
+    playerScore: PropTypes.number.isRequired,
+    highScore: PropTypes.number.isRequired,
+    // globalHighScore: PropTypes.number
+};
+
+export default GameInfo;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Game } from 'containers';
+
+
+
+
+
+const root = document.getElementById('root');
+
+
+render(
+    <Game boardSize={11} playerSize={25} />
+, root);
